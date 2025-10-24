4b8632771087bd4333ec12f2e2c0884f9d469caa
Ahmed Omran <ahmedamromran2003@gmail.com>
2025-10-24 16:29:26 +0300

    user and pass

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,17 @@ const nextConfig: NextConfig = {
   env: {
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    ADMIN_USERNAME: process.env.ADMIN_USERNAME,
+    ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
+    ADMIN_EMAIL: process.env.ADMIN_EMAIL,
+    ADMIN_BACKUP_EMAIL: process.env.ADMIN_BACKUP_EMAIL,
+    EMAIL_SERVICE: process.env.EMAIL_SERVICE,
+    EMAIL_USER: process.env.EMAIL_USER,
+    EMAIL_PASS: process.env.EMAIL_PASS,
+    EMAIL_FROM_NAME: process.env.EMAIL_FROM_NAME,
+    EMAIL_FROM_ADDRESS: process.env.EMAIL_FROM_ADDRESS,
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+    BLOB_READ_WRITE_TOKEN: process.env.BLOB_READ_WRITE_TOKEN,
   },
 };
 
